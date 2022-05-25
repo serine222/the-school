fa443f1cd9dabc03bddfb24c5fa5f2d59d6acf1b
serine222 <99805744+serine222@users.noreply.github.com>
2022-05-25 10:18:34 +0200

    Create grades

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-
+@section('title')
+{{trans('main_trans.Main_title')}}
+@stop
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,19 +10,21 @@
     <meta name="description" content="Webmin - Bootstrap 4 & Angular 5 Admin Dashboard Template" />
     <meta name="author" content="potenzaglobalsolutions.com" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600&display=swap" rel="stylesheet">
     @include('layouts.head')
 </head>
 
-<body>
+<body style="font-family: 'Cairo', sans-serif">
 
-    <div class="wrapper">
+    <div class="wrapper" style="font-family: 'Cairo', sans-serif">
 
         <!--=================================
  preloader -->
 
-        <div id="pre-loader">
-            <img src="assets/images/pre-loader/loader-01.svg" alt="">
-        </div>
+ <div id="pre-loader">
+     <img src="{{ URL::asset('assets/images/pre-loader/loader-01.svg') }}" alt="">
+ </div>
 
         <!--=================================
  preloader -->
@@ -33,10 +37,10 @@
  Main content -->
         <!-- main-content -->
         <div class="content-wrapper">
-            <div class="page-title">
+            <div class="page-title" >
                 <div class="row">
-                    <div class="col-sm-6">
-                        <h4 class="mb-0"> Dashboard</h4>
+                    <div class="col-sm-6" >
+                        <h4 class="mb-0" style="font-family: 'Cairo', sans-serif">{{trans('main_trans.Dashboard_page')}}</h4>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb pt-0 pr-0 float-left float-sm-right">
@@ -45,7 +49,7 @@
                 </div>
             </div>
             <!-- widgets -->
-            <div class="row">
+            <div class="row" >
                 <div class="col-xl-3 col-lg-6 col-md-6 mb-30">
                     <div class="card card-statistics h-100">
                         <div class="card-body">
